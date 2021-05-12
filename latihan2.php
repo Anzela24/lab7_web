c9042bdc715371caba118c7006a585d9fe7f0de7
Anzela24 <56473376+Anzela24@users.noreply.github.com>
2021-05-12 22:35:53 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<h1>predefine variable</h2>
+<body>
+
+	<?php
+	echo "Selamat Datang " .($_GET["nama"]) . '!';
+	?>
+</body>
+</html>
---
+++ php_dasar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DASAR</title>
+</head>
+<body>
+    <h1> belajar PHP dasar </h1> 
+    <?php 
+    echo "hello world";
+    ?>
+    <h2> menggunakan variabel </h2> 
+    
+    <?php
+   
+$nim = "311910663";
+$nama = 'ANZELA HASAN THALIA PUTRI';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+
+</body>
+</html>
